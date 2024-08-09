9492a171f87d7241f649741fb29f99e5070800e7
Iris Rademacher <irademac@broadinstitute.org>
2024-08-09 18:31:37 -0400

    hibi version of wilba m6 a pcb

---
+++ keyboards/wilba_tech/rama_works_m6_a_hibi/keymaps/default/keymap.c
@@ -0,0 +1,16 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    LAYOUT(
+        KC_MCTL, KC_MPLY, LGUI(KC_SPC), KC_MUTE, KC_VOLD, KC_VOLU),
+
+    LAYOUT(
+        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+
+    LAYOUT(
+        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+
+    LAYOUT(
+        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO) };
+
